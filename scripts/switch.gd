65579472241e61c4153cfa69fe0f81201ddf625f
Jorik Cronenberg <jorikcronenberg@mailbox.org>
2023-11-10 13:22:36 +0100

    Move cable points when moving switches

--- scripts/switch.gd
+++ scripts/switch.gd
@@ -4,7 +4,10 @@ var placed := false
 
 func _process(_delta):
 	if not placed:
+		var old_position = global_position
 		global_position = get_global_mouse_position().snapped(Vector2i(20, 20)) + Vector2(10, 10)
+		if old_position - global_position != old_position:
+			move_connected_cables(old_position - global_position)
 
 
 func _input(event):
@@ -16,3 +19,9 @@ func _input(event):
 		get_viewport().set_input_as_handled()
 	elif event.is_action_pressed("RClick") or event.is_action_pressed("Cancel"):
 		queue_free()
+
+
+func move_connected_cables(position_diff: Vector2):
+	for child in get_children():
+		if child.has_method("move_connected_cable"):
+			child.move_connected_cable(position_diff)
