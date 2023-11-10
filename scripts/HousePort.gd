a56607c1fd095247bdbd5f59445a38b57e69020c
Jorik Cronenberg <jorikcronenberg@mailbox.org>
2023-11-10 15:13:26 +0100

    Fix disconnect cable to update all deleted connections

--- scripts/HousePort.gd
+++ scripts/HousePort.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+class_name HousePort
+
 var cable_creator_scene = preload("res://scenes/cable_creator.tscn")
 const is_endpoint := true
 var connected_cable = null
@@ -17,5 +19,14 @@ func _input_event(_viewport, event, _shape_idx):
 		get_parent().set_is_port_connected(true)
 	elif event.is_action_pressed("LClick") and connected_cable and ui_controller.cursor_mode == "delete_cable":
 		get_node("/root/Main/Simulation").delete_cable(connected_cable)
-		connected_cable.queue_free()
-		connected_cable = null
+		# Need to store cable temporarily because otherwise we set connected_cable to null in disconnect()
+		# but we still need to queue_free() it
+		var tmp_cable = connected_cable
+
+		tmp_cable.port1.disconnect_port()
+		tmp_cable.port2.disconnect_port()
+		tmp_cable.queue_free()
+
+func disconnect_port():
+	connected_cable = null
+	get_parent().set_is_port_connected(false)
--- scripts/wan_port.gd
+++ scripts/wan_port.gd
@@ -1,5 +1,11 @@
 extends Area2D
 
+class_name WanPort
+
 const is_port_connected := false
 const is_endpoint := true
 const connected_cable = null
+
+# Nothing to do here
+func disconnect_port():
+	pass
